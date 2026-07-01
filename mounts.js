059f6495944870b81c8663455edce2e9a27adffa
Jawi <98896579+Jawi40@users.noreply.github.com>
2026-07-01 19:20:00 -0400

    Update mounts.js

--- mounts.js
+++ mounts.js
@@ -1,1 +1,25 @@
+export class Mount {
+  constructor(name) {
+    this.name = name;
+    this.source = null;
+    this.listeners = new Set();
+    this.buffer = [];
+    this.metadata = {};
+  }
+}
+
+const mounts = new Map();
+
+export function getOrCreateMount(name) {
+  if (!mounts.has(name)) mounts.set(name, new Mount(name));
+  return mounts.get(name);
+}
+
+export function broadcastChunk(mountName, chunk) {
+  const mount = mounts.get(mountName);
+  if (!mount) return;
+  for (const res of mount.listeners) {
+    res.write(chunk);
+  }
+}
 
